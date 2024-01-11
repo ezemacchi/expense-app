1f64caf8bea46ab2572e091d29b81ddc06839bf5
ezemacchi <emacchi@ext.laligatech.com>
2024-01-11 09:17:59 -0300

    Added firebase providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,26 @@ import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(APP_ROUTES), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"notion-app-integration","appId":"1:521072521202:web:66cbac9075602990a1c56b","databaseURL":"https://notion-app-integration-default-rtdb.firebaseio.com","storageBucket":"notion-app-integration.appspot.com","apiKey":"AIzaSyAQqQjtOwo83_IlNCxHLovmuJQTRW6VsJM","authDomain":"notion-app-integration.firebaseapp.com","messagingSenderId":"521072521202"}))), importProvidersFrom(provideDatabase(() => getDatabase()))
-  ]
+    provideRouter(APP_ROUTES),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'notion-app-integration',
+          appId: '1:521072521202:web:66cbac9075602990a1c56b',
+          databaseURL:
+            'https://notion-app-integration-default-rtdb.firebaseio.com',
+          storageBucket: 'notion-app-integration.appspot.com',
+          apiKey: 'AIzaSyAQqQjtOwo83_IlNCxHLovmuJQTRW6VsJM',
+          authDomain: 'notion-app-integration.firebaseapp.com',
+          messagingSenderId: '521072521202',
+        })
+      )
+    ),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };

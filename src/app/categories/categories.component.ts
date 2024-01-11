94bdced65f5874492117366ceecc8cc8cad8a25f
ezemacchi <emacchi@ext.laligatech.com>
2024-01-11 11:08:12 -0300

    Added basic usage of firabase

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,20 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { FirebaseService } from '../services/firabase.service';
 import { Category } from '../models/category';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+  styleUrl: './categories.component.css',
 })
 export class CategoriesComponent {
-
   private firebaseService: FirebaseService = inject(FirebaseService);
   categories: Category[] = [];
 
-  getAllCategories () {
-    this.firebaseService.GetCategories().subscribe(c => this.categories = c)
+  getAllCategories() {
+    this.firebaseService
+      .GetCategories()
+      .subscribe((c) => (this.categories = c));
   }
 }

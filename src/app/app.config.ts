481c54916f30e15d11417ebabfd4b9bdbda258b4
ezemacchi <emacchi@ext.laligatech.com>
2024-01-11 08:48:07 -0300

    Added AngularFire to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { APP_ROUTES } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(APP_ROUTES)
+    provideRouter(APP_ROUTES), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"notion-app-integration","appId":"1:521072521202:web:66cbac9075602990a1c56b","databaseURL":"https://notion-app-integration-default-rtdb.firebaseio.com","storageBucket":"notion-app-integration.appspot.com","apiKey":"AIzaSyAQqQjtOwo83_IlNCxHLovmuJQTRW6VsJM","authDomain":"notion-app-integration.firebaseapp.com","messagingSenderId":"521072521202"}))), importProvidersFrom(provideDatabase(() => getDatabase()))
   ]
 };

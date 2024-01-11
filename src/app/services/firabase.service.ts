914cc37ed07980679b65692874a304fa1348ce90
ezemacchi <emacchi@ext.laligatech.com>
2024-01-11 09:18:12 -0300

    Implemented GetCategories

--- src/app/services/firabase.service.ts
+++ src/app/services/firabase.service.ts
@@ -1,18 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor(private http: HttpClient) { }
+  private firestore: Firestore = inject(Firestore); // inject Cloud Firestore
 
-  /**
-   * GetExpenseCategories
-   */
+  constructor(private http: HttpClient, private store: Firestore) { }
+
   public GetCategories() {
-    
+    const categoryCollection = collection(this.firestore, 'categories');
+    return collectionData(categoryCollection) as Observable<Category[]>
   }
 
   public AddCategory() {
